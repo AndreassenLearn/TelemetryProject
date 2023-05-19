cff943ad184b286b330eeff357cfc6d394a41356
AndreassenLearn <simo531i@elevcampus.dk>
2023-05-19 17:36:49 +0200

    Add ServoControlView and edit layouts.

---
+++ MauiClient/ViewModels/ServoControlViewModel.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace MauiClient.ViewModels;
+
+public partial class ServoControlViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private int _minValue = 0;
+
+    [ObservableProperty]
+    private int _maxValue = 180;
+
+    [ObservableProperty]
+    private int _currentValue;
+
+    public ServoControlViewModel()
+    {
+        _currentValue = 0;
+    }
+
+    [RelayCommand]
+    public async void ValueChanged()
+    {
+
+    }
+}
---
+++ MauiClient/Views/ServoControlView.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiClient.Views;
+
+public partial class ServoControlView : ContentView
+{
+	public ServoControlView()
+	{
+		InitializeComponent();
+	}
+}
