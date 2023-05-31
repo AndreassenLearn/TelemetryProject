0e8ac3cb665f34d38f291cd75a96b74567d15aca
AndreassenLearn <simo531i@elevcampus.dk>
2023-05-31 18:42:04 +0200

    Change configuration.

--- TelemetryProject.CommonClient/Constants.cs
+++ TelemetryProject.CommonClient/Constants.cs
@@ -2,6 +2,6 @@
 
 public record Constants
 {
-    public static readonly string ApiBaseUrl = "https://1c6r45fd-7043.euw.devtunnels.ms/";
-    //public static readonly string ApiBaseUrl = "https://localhost:7043/";
+    //public static readonly string ApiBaseUrl = "https://75q64ksm-7043.euw.devtunnels.ms/";
+    public static readonly string ApiBaseUrl = "https://localhost:7043/";
 }
