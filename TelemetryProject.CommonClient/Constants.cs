8ca0be7b2ae3604a04ae41da400f34a60a6e365c
AndreassenLearn <simo531i@elevcampus.dk>
2023-06-30 16:38:10 +0200

    Cleaning.

--- TelemetryProject.CommonClient/Constants.cs
+++ TelemetryProject.CommonClient/Constants.cs
@@ -2,7 +2,7 @@
 
 public record Constants
 {
-    //public static readonly string ApiBaseUrl = "https://75q64ksm-7043.euw.devtunnels.ms/";
-    public static readonly string ApiBaseUrl = "https://localhost:7043/";
+    public static readonly string ApiBaseUrl = "https://k4rz598q-7043.euw.devtunnels.ms/";
+    //public static readonly string ApiBaseUrl = "https://localhost:7043/";
     //public static readonly string ApiBaseUrl = "http://telemetryproject.webapi/"; // Docker Compose
 }
